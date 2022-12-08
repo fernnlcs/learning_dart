44f78c701c95dfab81fcda3fea298522c7fcbbba
Fernando Silva <fernnlcs@gmail.com>
2022-12-08 14:23:00 -0300

    Class #4: Middleware

--- bin/learning_dart.dart
+++ bin/learning_dart.dart
@@ -5,8 +5,27 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
 
 void main(List<String> arguments) async {
-  final HttpServer server = await io.serve(_handler, '0.0.0.0', 4466);
+  Pipeline pipeline = Pipeline().addMiddleware(log());
+
+  final HttpServer server =
+      await io.serve(pipeline.addHandler(_handler), '0.0.0.0', 4466);
+
   print('Online at ${server.address.address}:${server.port}');
+}
+
+Middleware log() {
+  return (Handler handler) {
+    return (Request request) async {
+      // Before running
+      print('Requested: ${request.url}');
+
+      Response response = await handler(request);
+
+      // After running
+      print('Response: ${response.statusCode}');
+      return response;
+    };
+  };
 }
 
 FutureOr<Response> _handler(Request request) {
