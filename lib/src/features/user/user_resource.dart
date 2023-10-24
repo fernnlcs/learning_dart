d9d14c9ad80889df1e190a24e93ac54980c2fb38
Fernando Silva <fernnlcs@gmail.com>
2023-10-24 18:08:17 -0300

    Alguma coisa que eu fiz na época

--- lib/src/features/user/user_resource.dart
+++ lib/src/features/user/user_resource.dart
@@ -1,19 +1,24 @@
 import 'dart:async';
 
+import 'package:learning_dart/src/core/services/database/remote_database.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_modular/shelf_modular.dart';
 
 class UserResource extends Resource {
   @override
   List<Route> get routes => [
-    Route.get('/user', _getAllUsers),
-    Route.get('/user/:id', _getUserById),
-    Route.post('/user', _createUser),
-    Route.put('/user', _updateUser),
-    Route.delete('/user', _deleteUser),
-  ];
+        Route.get('/user', _getAllUsers),
+        Route.get('/user/:id', _getUserById),
+        Route.post('/user', _createUser),
+        Route.put('/user', _updateUser),
+        Route.delete('/user', _deleteUser),
+      ];
 
-  FutureOr<Response> _getAllUsers() {
+  FutureOr<Response> _getAllUsers(Injector injector) async {
+    final RemoteDatabase database = injector.get<RemoteDatabase>();
+    final List<Map<String, Map<String, dynamic>>> result =
+        await database.query('SELECT id, name, email, role from "User"');
+        
     return Response.ok('All users');
   }
 
